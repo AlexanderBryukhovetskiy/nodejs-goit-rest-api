f0987909e8273269a58be245aa9a6ce21fbf2604
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-04-11 22:56:11 +0300

    mongoose installed, DB connected (file server.js)

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+//-------------------------------------------
+const mongoose = require("mongoose");
+
+const PASSWORD = "RuTfwMzNOzdPF1E1";
+const DB_NAME = "contacts_finding";
+
+const DB_HOST = `mongodb+srv://Alexander:${PASSWORD}@cluster0.bfvvcci.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', true);   ///  ?
+
+mongoose.connect(DB_HOST)
+.then(()=> {
+  app.listen(3000);
+  console.log("Database connect success")
 })
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
+
+//--------------------------------------------
+
+
+
+
+
+
+
+//    RuTfwMzNOzdPF1E1
