5536d61575e9d2cf15b571a3e54b373de5eb5ca3
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-04-12 16:17:23 +0300

    config.js, .gitignore:  DB_HOST

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = require('./app')
 //-------------------------------------------
 const mongoose = require("mongoose");
 
-const PASSWORD = "RuTfwMzNOzdPF1E1";
-const DB_NAME = "contacts_finding";
+const {DB_HOST} = require('./config');
 
-const DB_HOST = `mongodb+srv://Alexander:${PASSWORD}@cluster0.bfvvcci.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+
 
 mongoose.set('strictQuery', true);   ///  ?
 
