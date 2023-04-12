dae916088c0fff6adb15f225c1b6c5b2e1ff00c7
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-04-12 16:57:32 +0300

    PORT add

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 const app = require('./app')
 
-//-------------------------------------------
 const mongoose = require("mongoose");
 
-const {DB_HOST} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
-mongoose.set('strictQuery', true);   ///  ?
+mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
 .then(()=> {
-  app.listen(3000);
+  app.listen(PORT);
   console.log("Database connect success")
 })
 .catch(error => {
   console.log(error.message);
   process.exit(1);
 });
-
-//--------------------------------------------
-
-
-
-
-
-
-
-//    RuTfwMzNOzdPF1E1
