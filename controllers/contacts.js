1a1d2a289285688fa14f2fb83e5df0ec55e7bf57
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-04-13 22:12:03 +0300

    schemas, Joi, delete json-files

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,55 +1,55 @@
 
-const contacts = require("../models/contacts");
+const Contact = require('../models/contact');
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-    const result = await contacts.listContacts();
+    const result = await Contact.find();
     res.json(result);
 };
 
-const getById =  async (req, res) => {
-    const contactId = req.params.contactId;
-    const result = await contacts.getById(contactId);
-    if(!result){
-      throw HttpError(404, "Not found");
-    }
-    res.json(result);
-};
+// const getById =  async (req, res) => {
+//     const contactId = req.params.contactId;
+//     const result = await contacts.getById(contactId);
+//     if(!result){
+//       throw HttpError(404, "Not found");
+//     }
+//     res.json(result);
+// };
 
 const add =  async (req, res) => {
-    const result = await contacts.addContact(req.body);
+    const result = await Contact.create(req.body);
     res.status(201).json(result);
 };
 
-const updateById = async (req, res) => {
-    if(Object.keys(req.body).length === 0){
-      throw HttpError(400, "missing fields");
-    }
+// const updateById = async (req, res) => {
+//     if(Object.keys(req.body).length === 0){
+//       throw HttpError(400, "missing fields");
+//     }
 
-    const {contactId} = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if(!result){
-      throw HttpError(404, "Not found");
-    }
-    res.status(200).json(result);
-};
+//     const {contactId} = req.params;
+//     const result = await contacts.updateContact(contactId, req.body);
+//     if(!result){
+//       throw HttpError(404, "Not found");
+//     }
+//     res.status(200).json(result);
+// };
 
-const deleteById = async (req, res) => {
-    const {contactId} = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({
-      "message": "contact deleted"
-    });
-};
+// const deleteById = async (req, res) => {
+//     const {contactId} = req.params;
+//     const result = await contacts.removeContact(contactId);
+//     if (!result) {
+//       throw HttpError(404, "Not found");
+//     }
+//     res.json({
+//       "message": "contact deleted"
+//     });
+// };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  getById: ctrlWrapper(getById),
+  // getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  updateById: ctrlWrapper(updateById),
-  deleteById: ctrlWrapper(deleteById)
+  // updateById: ctrlWrapper(updateById),
+  // deleteById: ctrlWrapper(deleteById)
 }
---
+++ models/contact.js
@@ -0,0 +1,60 @@
+const {Schema, model} = require('mongoose');
+
+const Joi = require('joi');
+
+const {handleMongooseError} = require('../helpers');
+
+//регулярний вираз для номера телефона:
+
+const phoneRegExp = /^\'('\d{3}'\')[\b]\d{3}-\d{2}-\d{2}$/ ;
+
+const contactSchema = new Schema({
+  name:{
+      type: String,
+      required: true,
+  },
+  email: {
+      type: String,
+      required: true,
+  },
+  phone: {
+      type: String,
+      // match: /^\$/,
+      required: true,
+  },
+  favorite: {
+      type: Boolean,
+      default: true,
+  }
+}, { versionKey: false, timestamps: true});
+
+contactSchema.post('save', handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().pattern(phoneRegExp).required(),
+  favorite: Joi.boolean().required(),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string().pattern(phoneRegExp),
+  favorite: Joi.boolean(),
+});
+
+
+
+const schemas = {
+  addSchema,
+  updateSchema,
+  //--- another schemas
+}
+
+const Contact = model('contact', contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
