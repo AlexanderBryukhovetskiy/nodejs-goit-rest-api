9ef5b2e7904ae0a3acafc08208107e811d7ef439
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-04-27 17:40:29 +0300

    multerConfig, upload

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const logger = require('morgan')
 const cors = require('cors')
 require("dotenv").config();
 
+//import packages: multer, path ---
+const multer = require('multer');
+const path = require('path');
+//---------------------------------
+
 const authRouter = require('./routes/api/auth');
 const contactsRouter = require('./routes/api/contacts');
 
@@ -13,6 +18,25 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+//create path to directory for temporary saving pictures:
+const tempDir = path.join(__dirname, 'temp');
+
+//create config.object for multer: write the path to directory for temporary saving pictures:
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  //OPTIONAL: function for naming files (00:26:00)
+  filename: (req, file, cb) => {
+    cb( null, file.originalname);
+  }
+})
+
+//middleware for saving files:
+const upload = multer({
+  storage: multerConfig
+});
+
+//-----------------------
 
 app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
