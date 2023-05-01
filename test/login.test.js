33984b64c21e933879c2a8d87e4ada47bd256666
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2023-05-01 22:32:36 +0300

    tests rewrited

--- test/login.test.js
+++ test/login.test.js
@@ -17,17 +17,44 @@ describe('login', () => {
 
   afterAll( async() => {
     await mongoose.disconnect(DB_HOST);
+    
   });
 
-  it('should login user', async () => {
+  it('should return a response with code status 200', async () => {
       const response = await supertest(app).post('/api/auth/login').send({
         email: "anna@mail.com",
         password: '123456',
       });
       expect(response.statusCode).toBe(200);
-      expect(response.body.user.email).toEqual("anna@mail.com");
-      expect(response.body.user.subscription).toEqual("starter");
-      expect(response.body.token).not.toBeFalsy();
+  });
+
+    
+  it('should return in response a token', async () => {
+    const response = await supertest(app).post('/api/auth/login').send({
+      email: "anna@mail.com",
+      password: '123456',
+    });
+    expect(response.body.token).not.toBeFalsy();
+  });
+
+  it('should return in response an object user with 2 fields: email and subscription', async () => {
+    const response = await supertest(app).post('/api/auth/login').send({
+      email: "anna@mail.com",
+      password: '123456',
+    });
+    expect(typeof response.body.user).toBe("object");
+    expect(response.body.user.email).toEqual("anna@mail.com");
+    expect(response.body.user.subscription).toEqual("starter");
+  });
+
+  it('email and subscription data type in response is "string"', async () => {
+    const response = await supertest(app).post('/api/auth/login').send({
+      email: "anna@mail.com",
+      password: '123456',
+    });
+    expect(typeof response.body.user.email).toBe("string");
+    expect(typeof response.body.user.subscription).toBe("string");
+    expect(response.body.token).not.toBeFalsy();
   });
 });
 
